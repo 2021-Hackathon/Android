73f18bcdac616a6a4ac95305674cbdedec57c508
tmddusqls <tmddusqls@naver.com>
2021-12-29 17:36:13 +0900

    HomeSurveyFragment

--- app/src/main/java/com/example/a2021hackthon/view/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/a2021hackthon/view/adapter/ViewPagerAdapter.kt
@@ -3,15 +3,18 @@ package com.example.a2021hackthon.view.adapter
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.a2021hackthon.view.fragment.HomeFaceFragment
+import com.example.a2021hackthon.view.fragment.HomeSurveyFragment
 import com.example.a2021hackthon.view.fragment.HomeTimeFragment
+import com.example.a2021hackthon.view.fragment.SurveyFragment
 
 class ViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 2
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment =
         when (position) {
             0 -> HomeFaceFragment()
             1 -> HomeTimeFragment()
+            2 -> HomeSurveyFragment()
             else -> HomeFaceFragment()
         }
 
