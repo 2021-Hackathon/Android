2eeaf79bb89a7efecd9791a76479d7c13db6d2e2
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-12-29 18:50:20 +0900

    [EDIT] Intro

--- app/src/main/java/com/example/a2021hackthon/view/fragment/IntroFragment.kt
+++ app/src/main/java/com/example/a2021hackthon/view/fragment/IntroFragment.kt
@@ -24,7 +24,7 @@ class IntroFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.nextButton.setOnClickListener {
+        binding.start.setOnClickListener {
             findNavController().navigate(R.id.action_introFragment2_to_homeFragment)
         }
     }
